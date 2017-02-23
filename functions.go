f8026bc093fa4a6b350f27d73d8a94a0d604ecd7
Rob Porter <rjpdesigns@gmail.com>
2017-02-23 14:06:17 +0000

    Added Encrypt/Decrypt functions

--- functions.go
+++ functions.go
@@ -1,7 +1,12 @@
 package functions
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"html/template"
+	"io"
 	"os"
 	"strings"
 )
@@ -47,4 +52,47 @@ func RemoveDuplicates(a []string) []string {
 		}
 	}
 	return result
+}
+
+func encodeBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func decodeBase64(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func Encrypt(key, text []byte) string {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	ciphertext := make([]byte, aes.BlockSize+len(text))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], text)
+	return encodeBase64(ciphertext)
+}
+
+func Decrypt(key []byte, b64 string) string {
+	text := decodeBase64(b64)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	if len(text) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := text[:aes.BlockSize]
+	text = text[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(text, text)
+	return string(text)
 }
