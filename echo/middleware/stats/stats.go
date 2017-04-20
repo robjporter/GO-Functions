e398a5d33a3f6fb297a277c357f21c9e0958dec8
robjporter <robporter79@outlook.com>
2017-04-20 14:39:39 +0100

    Updated stats module

--- echo/middleware/stats/stats.go
+++ echo/middleware/stats/stats.go
@@ -7,21 +7,32 @@ import (
 	"time"
 
 	"github.com/labstack/echo"
+	"github.com/robjporter/go-functions/browser"
 )
 
 type (
 	Stats struct {
-		Uptime       time.Time      `json:"uptime"`
-		RequestCount uint64         `json:"requestCount"`
-		Statuses     map[string]int `json:"statuses"`
-		mutex        sync.RWMutex
+		Uptime         time.Time      `json:"uptime"`
+		RequestCount   uint64         `json:"requestCount"`
+		Statuses       map[string]int `json:"statuses"`
+		Browsers       map[string]int `json:"browsers"`
+		BrowserVersion map[string]int `json:"browserversion"`
+		OS             map[string]int `json:"os"`
+		Device         map[string]int `json:"device"`
+		OSVersion      map[string]int `json:"osversion"`
+		mutex          sync.RWMutex
 	}
 )
 
 func NewStats() *Stats {
 	return &Stats{
-		Uptime:   time.Now(),
-		Statuses: map[string]int{},
+		Uptime:         time.Now(),
+		Statuses:       map[string]int{},
+		Browsers:       map[string]int{},
+		BrowserVersion: map[string]int{},
+		OS:             map[string]int{},
+		Device:         map[string]int{},
+		OSVersion:      map[string]int{},
 	}
 }
 
@@ -35,9 +46,20 @@ func (s *Stats) Process(next echo.HandlerFunc) echo.HandlerFunc {
 		defer s.mutex.Unlock()
 		s.RequestCount++
 		status := strconv.Itoa(c.Response().Status)
+		browser, browserversion, devicename, osname, osversion := getBrowser(c.Request().UserAgent())
 		s.Statuses[status]++
+		s.Browsers[browser]++
+		s.BrowserVersion[browser+" "+browserversion]++
+		s.OS[osname]++
+		s.OSVersion[osname+" "+osversion]++
+		s.Device[devicename]++
 		return nil
 	}
+}
+
+func getBrowser(agent string) (string, string, string, string, string) {
+	ua1 := browser.Parse(agent)
+	return ua1.Browser.Name, ua1.Browser.Version, ua1.Device.Name, ua1.OS.Name, ua1.OS.Version
 }
 
 // Handle is the endpoint to get stats.
@@ -45,12 +67,4 @@ func (s *Stats) Handle(c echo.Context) error {
 	s.mutex.RLock()
 	defer s.mutex.RUnlock()
 	return c.JSON(http.StatusOK, s)
-}
-
-// ServerHeader middleware adds a `Server` header to the response.
-func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		c.Response().Header().Set(echo.HeaderServer, "Echo/3.0")
-		return next(c)
-	}
 }
